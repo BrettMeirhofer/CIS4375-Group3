9e1a5957628ed9f2a9168cfe4ddb492d916bf59c
sethesparza <seth.i.esparza@gmail.com>
2023-03-31 20:31:13 -0500

    Update data_dict_helper.py

--- Backend/group3site/jeans/data_dict_helper.py
+++ Backend/group3site/jeans/data_dict_helper.py
@@ -222,3 +222,30 @@ def generate_insert_sql(app_name):
         drop_file.write("GO\n")
         drop_file.write("\n")
     drop_file.close()
+
+def send_promo_email(app_name, email_list):
+    gmail_user = 'jeansyfajaspromos@gmail.com'
+    gmail_password = 'hnhzjfajwiorxuju'
+
+    sent_from = gmail_user
+    to = [email_list]
+    subject = 'Test for Promotional Emails'
+    body = 'This is a test'
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(gmail_user, gmail_password)
+        smtp_server.sendmail(sent_from, to, email_text)
+        smtp_server.close()
+        print ("Email sent successfully!")
+    except Exception as ex:
+        print ("Something went wrong….",ex)
