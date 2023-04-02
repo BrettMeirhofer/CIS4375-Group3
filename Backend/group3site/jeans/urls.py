3d73689bbe88c530c44a879e3358777b93ce4912
permsingh10 <tomsingh2011@gmail.com>
2023-04-02 13:10:26 -0500

    Reports Functionality
    Added the basic reports function, this included top promos being used by customers and top customers using promos. There is also a print reports function.

--- Backend/group3site/jeans/urls.py
+++ Backend/group3site/jeans/urls.py
@@ -58,5 +58,7 @@ urlpatterns = [
     path('graph', views.best_cust_month, name='graph'),
     path('graph2', views.best_promo_month, name='graph2'),
     path('graph3', views.uniq_cust_month, name='graph3'),
-    path('preview_promo/', views.preview_promo, name='preview_promo')
+    path('preview_promo/', views.preview_promo, name='preview_promo'),
+    path('report/', views.top_promos, name='report'),
+   
 ]
