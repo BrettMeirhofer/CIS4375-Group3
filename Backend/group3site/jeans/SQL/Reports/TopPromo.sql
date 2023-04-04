5311290b2c35ab28b347cb6e47888cdafda19042
permsingh10 <tomsingh2011@gmail.com>
2023-04-03 21:10:51 -0500

    Reports Page
    Reports page func has been changed to run sql queries, Changed the report page to have two buttons  the links top promos and top customers

--- Backend/group3site/jeans/SQL/Reports/TopPromo.sql
+++ Backend/group3site/jeans/SQL/Reports/TopPromo.sql
@@ -1,1 +1,16 @@
-SELECT * FROM Promo
+SELECT
+    promo.id,
+    promo.promo_name,
+    promo.promo_code,
+    promo.promo_desc,
+    COUNT(DISTINCT CustomerPromo.customer_id) AS customer_count
+FROM
+    Promo promo
+INNER JOIN CustomerPromo ON promo.id = CustomerPromo.promo_id
+GROUP BY
+    promo.id,
+    promo.promo_name,
+    promo.promo_code,
+    promo.promo_desc
+ORDER BY
+    customer_count DESC;
--- Backend/group3site/jeans/urls.py
+++ Backend/group3site/jeans/urls.py
@@ -59,6 +59,8 @@ urlpatterns = [
     path('graph2', views.top5_promos, name='graph2'),
     path('graph3', views.uniq_cust_month, name='graph3'),
     path('preview_promo/', views.preview_promo, name='preview_promo'),
-    path('report/', views.top_promos, name='report'),
+    path('report/', views.report, name='report'),
+    path('top_promos/', views.top_promos, name='top_promos'),
+    path('top_customers/', views.top_customers, name='top_customers'),
    
 ]
