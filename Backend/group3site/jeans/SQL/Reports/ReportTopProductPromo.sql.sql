55d0bf60bd047111f8c634f95a36982d23cd4f8a
permsingh10 <tomsingh2011@gmail.com>
2023-04-07 21:06:59 -0500

    Added more reports
    Most Used Products by promo redeems, Unique Customers per month, Returning Customers per month. Made sure to follow the meta data process for the report handling process.

---
+++ Backend/group3site/jeans/SQL/Reports/ReportTopProductPromo.sql.sql
@@ -0,0 +1,18 @@
+-- Owner: Perminder Singh
+-- Top Product Promo
+-- Rule: Most Promotions Used
+-- Description: This report shows the top products with the most promotions used, along with the promotion names.
+-- Product Name, Promo Count, Promos Used
+-- left, right, left
+SELECT 
+  p.product_name, 
+  COUNT(*) AS promo_count,
+  STRING_AGG(pr.promo_name, ', ') AS promos_used
+FROM 
+  Product p
+  JOIN ProductPromo pp ON pp.product_id = p.id
+  JOIN Promo pr ON pr.id = pp.promo_id
+GROUP BY 
+  p.product_name
+ORDER BY 
+  promo_count DESC;
