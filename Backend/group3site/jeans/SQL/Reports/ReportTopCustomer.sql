ee5ff35cc21c48ea1755f61ef2a8bbb90f572232
permsingh10 <tomsingh2011@gmail.com>
2023-04-10 21:35:47 -0500

    updated reports

--- Backend/group3site/jeans/SQL/Reports/ReportTopCustomer.sql
+++ Backend/group3site/jeans/SQL/Reports/ReportTopCustomer.sql
@@ -2,23 +2,27 @@
 --Top Customers by Promo Engagement
 --The client can use this report as one metric to access cashier performance. Cashiers with high customer spending may have skills that could be transferred to lower performing Cashiers increasing the overall revenue of the business.
 --Displays all the active cashiers and the total amount of customer spending with them acting as the cashier.
---Row Number, Customer ID, Full Name, Email Address, Promos Redeemed
---,,,,
+--Customer ID, Full Name, Email Address, Total Income, Total Discount, Num Products Purchased, Num Products Redeemed
+--left, left, left, money, money, left, left
 
 SELECT
-    ROW_NUMBER() OVER(ORDER BY (SELECT NULL)),
     Customer.id AS customer_id,
-    Customer.first_name + ' ' + Customer.last_name,
+    Customer.first_name + ' ' + Customer.last_name AS customer_name,
     Customer.email,
-    COUNT(CustomerPromo.promo_id) AS promo_count
+    SUM(CustomerProductPromo.line_total) AS total_income,
+    SUM(CustomerProductPromo.line_discount) AS total_discount,
+    SUM(CustomerProductPromo.quantity) AS num_products_purchased,
+    COUNT(CustomerPromo.id) AS num_promo_redeemed
 FROM
     Customer
 INNER JOIN CustomerPromo ON Customer.id = CustomerPromo.customer_id
+INNER JOIN CustomerProductPromo ON CustomerPromo.id = CustomerProductPromo.customer_promo_id
 WHERE CustomerPromo.created_date BETWEEN '{}' AND '{}'
 GROUP BY
     Customer.id,
     Customer.first_name,
     Customer.last_name,
-	Customer.email
+    Customer.email
 ORDER BY
-    promo_count DESC;
+    num_promo_redeemed DESC;
+
